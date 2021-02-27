53b326caf9ee67f78c720f0c78d8d59bb8137f76
alexcorsau <77393961+alexcorsau@users.noreply.github.com>
2021-02-27 22:32:02 +0200

    async-await response from local json file

--- index.js
+++ index.js
@@ -2,33 +2,43 @@ console.log("JavaScript - AJAX");
 
 const articleListHtml = document.querySelector(".article-list");
 
-document.getElementById("get-data").addEventListener("click",function (){
-    console.log("Before Fetch");
-    fetch("https://simple-json-server-scit.herokuapp.com/posts")
-        .then(handleFetchResponse)
-        .then(useJSONResponse);
-    console.log("After Fetch");
-});
+// document.getElementById("get-data").addEventListener("click",function (){
+//     console.log("Before Fetch");
+//     fetch("http://127.0.0.1:5501/testingJSON.json")
+//         .then(handleFetchResponse)
+//         .then(useJSONResponse);
+//     console.log("After Fetch");
+// });
+
+//more Readability??
+
+document.getElementById("get-data").addEventListener("click",onClick);
+
+async function onClick(){
+    let responseFromFile = await (await fetch("http://127.0.0.1:5501/testingJSON.json")).json();
+    console.log("Line 19",responseFromFile);
+    // let jsonObject = await responseFromFile.json();
+    useJSONResponse(responseFromFile);
+}
 
 
 function handleFetchResponse(response){
-    console.log("response:",response);
+    console.log("Line 31:",response);
     return response.json();
 }
 
 function useJSONResponse(json) {
-    console.log(json);
+    console.log("Line 36: ",json);
     renderArticles(json);
 }
 
 function renderArticles(articleList){
     articleListHtml.innerText = "";
     for (const articleData of articleList) {
-        console.log(articleData);
+        console.log("Line 43:",articleData);
         renderArticle(articleData);
     }
 }
-
 
 
 function renderArticle(articleData){
