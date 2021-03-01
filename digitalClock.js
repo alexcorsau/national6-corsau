0be4ca3accd8332558abae57ed748a66c6af69b5
alexcorsau <77393961+alexcorsau@users.noreply.github.com>
2021-03-02 00:47:57 +0200

    finished homework

--- digitalClock.js
+++ digitalClock.js
@@ -12,7 +12,11 @@ const minutesParagraphs = document.querySelectorAll(".minutes p");
 let hours = 0;
 const hoursParagraphs = document.querySelectorAll(".hours p");
 
-setInterval(function () {
+let savedTimersDivision = document.querySelector(".Saved-timers");
+
+let statusRunning = false;
+
+function incrementTime() {
   renderDigits(seconds, secondsParagraphs);
   renderDigits(minutes, minutesParagraphs);
   renderDigits(hours, hoursParagraphs);
@@ -32,7 +36,7 @@ setInterval(function () {
   if (hours === 24) {
     hours = 0;
   }
-}, 1000);
+}
 
 function renderDigits(nr, pList) {
   const stringDigits = nr + "";
@@ -45,4 +49,50 @@ function renderDigits(nr, pList) {
     pList[0].innerText = 0;
     pList[1].innerText = digitList[0];
   }
+}
+
+
+let timer;
+let counterOfSavedTimers=0;
+
+document.getElementById("start-button").addEventListener("click",startTimer);
+document.getElementById("stop-button").addEventListener("click",stopTimer);
+document.getElementById("reset-button").addEventListener("click",resetTimer);
+document.getElementById("save-timer").addEventListener("click",saveTimer);
+
+function startTimer() {
+  if(statusRunning===false) {
+    timer = setInterval(incrementTime,1000);
+    statusRunning=true;
+  }
+}
+
+function stopTimer() {
+  if(statusRunning===true) {
+    clearInterval(timer);
+    statusRunning=false;
+  }
+}
+
+function resetTimer() {
+  stopTimer();
+  seconds=0;
+  minutes=0;
+  hours=0;
+  renderDigits(seconds, secondsParagraphs);
+  renderDigits(minutes, minutesParagraphs);
+  renderDigits(hours, hoursParagraphs);
+  counterOfSavedTimers=0;
+  clearSavedTimers();
+}
+
+function saveTimer() {
+  counterOfSavedTimers++;
+  const newTimerSaved = document.createElement("p");
+  newTimerSaved.innerText = `Timer ${counterOfSavedTimers}: ${hoursParagraphs[0].innerText}${hoursParagraphs[1].innerText}:${minutesParagraphs[0].innerText}${minutesParagraphs[1].innerText}:${secondsParagraphs[0].innerText}${secondsParagraphs[1].innerText}`;
+  savedTimersDivision.appendChild(newTimerSaved);
+}
+
+function clearSavedTimers() {
+  savedTimersDivision.innerHTML="";
 }
