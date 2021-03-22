a68479f1c4ec8dbc34de78ad23d4592b689d10a0
alexcorsau <77393961+alexcorsau@users.noreply.github.com>
2021-03-22 21:52:18 +0200

    starting point

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Spaceship Homework - Alex Corsau");
+
+document.getElementById("generate-starship").addEventListener("click",generateStarship());
+
+function generateStarship(){
+    
+}
