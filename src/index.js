57a835b9ba8e8d412370a0e943096c714c829962
alexcorsau <77393961+alexcorsau@users.noreply.github.com>
2021-04-06 20:52:45 +0300

    finish-lesson

---
+++ src/index.js
@@ -0,0 +1,8 @@
+console.log("Entry point to webpack");
+import { addPixel } from "./utils/addUnitsType"
+
+const aRandomNumber = Math.floor(Math.random()*20);
+
+console.log(aRandomNumber);
+const aRandomNumberWithPixel = addPixel(aRandomNumber);
+console.log(aRandomNumberWithPixel);
