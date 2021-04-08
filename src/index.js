238e6e414c1c82c2a1692f7ca81c042ac7eca651
alexcorsau <77393961+alexcorsau@users.noreply.github.com>
2021-04-08 20:42:42 +0300

    clean start

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-console.log("Entry point to webpack");
-import { addPixel } from "./utils/addUnitsType"
-
-const aRandomNumber = Math.floor(Math.random()*20);
-
-console.log(aRandomNumber);
-const aRandomNumberWithPixel = addPixel(aRandomNumber);
-console.log(aRandomNumberWithPixel);
+console.log("To Do app");
