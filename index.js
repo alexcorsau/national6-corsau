a1d3eaf6e3a7a0ceaa036ecee46aedeebdd4931e
alexcorsau <77393961+alexcorsau@users.noreply.github.com>
2021-04-11 18:49:40 +0300

    refactoring step 1

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-// const { toInteger } = require("");
-
-
 console.log("JavaScript - Dogs App");
-let breedListArray =[];
+
 let currentBreedPictures = [];
 
 if (!localStorage.getItem("name") || !localStorage.getItem("password")) {
   window.location = "/login.html";
 }
-
-
 
 document.getElementById("logout").addEventListener("click", () => {
   localStorage.removeItem("name");
@@ -19,24 +14,16 @@ document.getElementById("logout").addEventListener("click", () => {
   window.location = "/";
 });
 
-
 fetch("https://dog.ceo/api/breeds/list/all")
 .then(handleFetchResponse)
 .then(renderBreeds)
 
-
 function handleFetchResponse(response) {
-  // console.log("response from server: ", response);
   return response.json();
 }
 
 function renderBreeds(breedList) {
-  breedListArray=Object.keys(breedList.message);
-  // console.log(breedListArray);
-  // console.log("Object.values() breedList.message: ",Object.values(breedList.message));
-  // console.log("typeOf Object.values(breedList.message) : ",typeof(Object.values(breedList.message)));
   for (const breed of Object.keys(breedList.message)) {
-    console.log(breed);
     renderBreed(breed);
   }
   if(localStorage.breed && localStorage.index) {
@@ -54,7 +41,6 @@ function renderBreed(dogBreed){
   breedName.innerText = dogBreed;
   breedName.setAttribute("id",dogBreed);
   breedName.addEventListener("click",selectBreed);
-  console.log(breedName);
   document.getElementById("breeds").appendChild(breedName);
 }
 
@@ -64,65 +50,41 @@ function selectBreed(){
   document.getElementById("page-number").innerText = "";
   document.getElementById("page-number").innerText = parseInt(localStorage.index)+1;
   localStorage.setItem("breed",this.id);
-  // localStorage.setItem("index",);
   this.classList.add("breed--selected");
-
   fetch(`https://dog.ceo/api/breed/${this.id}/images`)
     .then(handleFetchResponse)
     .then(renderImage)
 }
 
-
 function renderImage(imageResponseMessage) {
   currentBreedPictures = imageResponseMessage.message;
-  console.log(currentBreedPictures);
-  // console.log("imageResponseMessage: ",imageResponseMessage.message);
-  // console.log("typeOf imageResponseMessage : ",typeof(imageResponseMessage.message));
-
-  // console.log("imageResponseMessage[0]: ",imageResponseMessage.message[267]);
-  // console.log("typeOf imageResponseMessage[0] : ",typeof(imageResponseMessage.message[267]));
   if(localStorage.index) {
     document.getElementById("breed-image").setAttribute("src",currentBreedPictures[localStorage.index]);
     }
     else document.getElementById("breed-image").setAttribute("src",currentBreedPictures[0]);
-  // document.getElementById("breed-image").setAttribute("src",imageResponseMessage.message[267]);
 }
-
-// function showImage(imageLink){
-//   document.getElementById("breed-img").setAttribute("src",imageLink);
-// }
-
 
 document.getElementById("backward").addEventListener("click",showPreviousImage);
 document.getElementById("forward").addEventListener("click",showNextImage);
 
 function showPreviousImage(){
-  if(localStorage.index>=1) {
-    localStorage.index--;
+  if(document.querySelector(".breed-selected")){
+    if(localStorage.index>=1) {
+      localStorage.index--;
+    }
+    document.getElementById("page-number").innerText = "";
+    document.getElementById("page-number").innerText = parseInt(localStorage.index)+1;
+    document.getElementById("breed-image").setAttribute("src",currentBreedPictures[localStorage.index]);
   }
-  document.getElementById("page-number").innerText = "";
-  document.getElementById("page-number").innerText = parseInt(localStorage.index)+1;
-  document.getElementById("breed-image").setAttribute("src",currentBreedPictures[localStorage.index]);
-
-  // if(!localStorage.index){
-  //   localStorage.index=0;
-  //   document.getElementById("page-number").innerText = "";
-  //   document.getElementById("page-number").innerText = parseInt(localStorage.index)+1;
-  //   } else  {
-  //     if(localStorage.index===0) {
-  //       localStorage.index--;
-  //     }
-  //     document.getElementById("page-number").innerText = "";
-  //     document.getElementById("page-number").innerText = parseInt(localStorage.index)+1;
-  //     document.getElementById("breed-image").setAttribute("src",currentBreedPictures[localStorage.index]);
-  //   }
 }
 
 function showNextImage(){
-  if(localStorage.index<currentBreedPictures.length-1) {
+  if(document.querySelector(".breed-selected")){
+    if(localStorage.index<currentBreedPictures.length-1) {
       localStorage.index++;
     }
     document.getElementById("page-number").innerText = "";
     document.getElementById("page-number").innerText = parseInt(localStorage.index)+1;
     document.getElementById("breed-image").setAttribute("src",currentBreedPictures[localStorage.index]);
+  } 
 }
