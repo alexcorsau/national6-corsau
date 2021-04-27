3181f009d84c6f040b9589650ed2acde081fb701
alexcorsau <77393961+alexcorsau@users.noreply.github.com>
2021-04-27 21:04:42 +0300

    course finish

--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -6,7 +6,15 @@ export function ToDoItem(props){
     console.log(props);
     return(
         <div className="to-do-item">
-            <input type="checkbox" defaultChecked={props.checkValue}></input>
+            <input 
+                type="checkbox"
+                defaultChecked={props.checkValue}
+                onChange={()=> {
+                    props.updateCheckStatus(props.itemIndex,!props.checkValue)
+                }}
+            >
+
+            </input>
             <p>{props.label}</p>
             <img id="trash" src={trashCan} alt="trash can" onClick={()=>props.removeItem(props.label)}></img>
         </div>
--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,46 +1,124 @@
 import { Component } from "react";
 import { ToDoItem } from "../ToDoItem/ToDoItem";
 
-const mockToDoListData = [
-    {
-        checked: false,
-        item: "task1"
-    },
-    {
-        checked: true,
-        item: "task2"
-    },
-    {
-        checked: false,
-        item: "task3"
-    }
-];
-
 export class ToDoList extends Component {
     state = {
-        toDoList : mockToDoListData,
+        toDoList : [],
         inputValue:"",
+        shouldCreateUser: false,
+        inputIsValid: true,
     }
 
     removeItem=(itemText)=>{
-        this.setState({toDoList: this.state.toDoList.filter(itemData => itemData.item!==itemText)});
+        const filteredToDoList = this.state.toDoList.filter(itemData => itemData.item!==itemText);
+        this.setState({toDoList: filteredToDoList});
+        const payload= {
+            id:"acorsau",
+            todo:filteredToDoList
+            };
+        fetch("https://simple-json-server-scit.herokuapp.com/todo/acorsau",{
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        });
     }
 
     handleInputChange = (event)=> {
-        this.setState({inputValue: event.target.value})
+        this.setState({inputValue: event.target.value});
+        if(this.state.inputValue!=="") {
+            this.setState({inputIsValid:true});
+        } else {
+            this.setState({inputIsValid:false});
+        }
     }
 
     handleAddItem = ()=>{
+        if(this.state.inputValue!==""){
         this.setState({
             toDoList: [
                 {checked:false, item:this.state.inputValue},
                 ...this.state.toDoList
             ],
-            inputValue:""
+            inputValue:"",
+            inputIsValid:true,
+        });
+        const payload= {
+            id:"acorsau",
+            todo:[{checked:false, item:this.state.inputValue},
+                ...this.state.toDoList]
+            };
+       
+            if(this.state.shouldCreateUser) {
+                fetch("https://simple-json-server-scit.herokuapp.com/todo",{
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(payload)
+                })
+                this.setState({shouldCreateUser: false});
+            } else {
+                    fetch("https://simple-json-server-scit.herokuapp.com/todo/acorsau",{
+                    method: "PUT",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(payload)
+                })
+                }
+        } else {
+            this.setState({inputIsValid:false});
+        }
+        
+    }
+
+    updateCheckStatus=(index,value)=>{
+        console.log(index,value);
+        const newList = this.state.toDoList.map((item,itemIndex)=>{
+            if(itemIndex===index) {
+                item.checked = value;
+            }
+            return item;
         })
+        this.setState({toDoList:newList});
+
+        const payload= {
+            id:"acorsau",
+            todo:newList
+            };
+        fetch("https://simple-json-server-scit.herokuapp.com/todo/acorsau",{
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        });
+    }
+
+    componentDidMount(){
+        console.log("ToDo List has finished mounting");
+        fetch("https://simple-json-server-scit.herokuapp.com/todo/acorsau")
+            .then((r)=>r.json())
+            .then((json) => {
+                console.log(json);
+                if(json.todo) {
+                    this.setState({toDoList:json.todo})
+                } else {
+                    this.setState({shouldCreateUser: true})
+                }  
+            }
+        )
+    }
+
+    componentDidUpdate(){
+        console.log("ToDo List has Updated");
     }
 
     render(){
+        let addItemClassName = "";
+        if(!this.state.inputIsValid) addItemClassName+=" add-item--invalid";
         return(
             <div className="to-do-list">
                 {
@@ -49,11 +127,14 @@ export class ToDoList extends Component {
                         key={itemData.item}
                         checkValue={itemData.checked} 
                         label={itemData.item} 
-                        removeItem={this.removeItem}/>
+                        removeItem={this.removeItem}
+                        itemIndex ={index}
+                        updateCheckStatus={this.updateCheckStatus}
+                        />
                         ))
                 }
-                <div className="add-item">
-                    <input value={this.state.inputValue} onChange={this.handleInputChange}/>
+                <div>
+                    <input className={addItemClassName} value={this.state.inputValue} onChange={this.handleInputChange}/>
                     <button onClick={this.handleAddItem}>+</button>
                 </div>
             </div>
