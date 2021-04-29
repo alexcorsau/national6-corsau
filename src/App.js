9cc3b679f463d2d9d5107e0bffd4a506d7af8008
alexcorsau <77393961+alexcorsau@users.noreply.github.com>
2021-04-29 19:05:08 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import { Header } from "./components/Header/Header";
 import { ToDoList } from "./components/ToDoList/ToDoList";
 
 import './App.css';
+import { Component } from "react";
 
+class App extends Component {
+  state = {
+    showToDoList: true,
+  };
 
+  toggleToDoListVisibility = () => {
+    this.setState({ showToDoList: !this.state.showToDoList });
+  };
 
-
-function App() {
-  return (
-    <div className="App" id="app">
-      <Header/>
-      <ToDoList/>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App" id="app">
+        <Header />
+        <button onClick={this.toggleToDoListVisibility}>
+          Hide/Show ToDoList
+        </button>
+        {this.state.showToDoList ? <ToDoList /> : null}
+      </div>
+    );
+  }
 }
 
 export default App;
